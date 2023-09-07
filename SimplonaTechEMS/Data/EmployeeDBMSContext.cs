1880d3eabe15c9e1e0702647d46a64672c13ad6d
PrashastVats1 <prashastvats@gmail.com>
2023-09-07 16:37:49 +0530

    Using Web Core API to create a DB for SimplonaTech

---
+++ SimplonaTechEMS/Data/EmployeeDBMSContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SimplonaTechEMS.Models;
+
+namespace SimplonaTechEMS.Data
+{
+    public class EmployeeDBMSContext : DbContext
+    {
+        public EmployeeDBMSContext (DbContextOptions<EmployeeDBMSContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SimplonaTechEMS.Models.DeptMaster> DeptMaster { get; set; } = default!;
+
+        public DbSet<SimplonaTechEMS.Models.EmpProfile>? EmpProfile { get; set; }
+    }
+}
